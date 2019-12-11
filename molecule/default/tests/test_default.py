187d78245af76ae8909b203e9b47483c17c08a07
David Norman <deekayen@deekayen.net>
2019-12-11 16:39:05 -0500

    Add newlines to the molecule default tests for flake8.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,16 +11,19 @@ def test_httpd_installed(host):
 
     assert apache.is_installed
 
+
 def test_httpd_listener(host):
     listener = host.socket("tcp://0.0.0.0:80")
 
     assert listener.is_listening
+
 
 def test_httpd_service(host):
     service = host.service("httpd")
 
     assert service.is_enabled
     assert service.is_running
+
 
 def test_httpd_path(host):
     path = host.file("/etc/httpd")
