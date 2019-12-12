2a8822f38bf0cda2a44fee007ac7b23a6b4bc893
David Norman <deekayen@deekayen.net>
2019-12-12 14:18:22 -0500

    merge test files. http.py didn't get executed on its own.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,3 +1,4 @@
+import http.client
 import os
 import testinfra.utils.ansible_runner
 
@@ -6,22 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_apache2_installed(host):
-    apache = host.package("apache2")
-
-    assert apache.is_installed
-
-
-def test_apache2_listener(host):
-    listener = host.socket("tcp://0.0.0.0:80")
-
-    assert listener.is_listening
-
-
-def test_apache2_service(host):
-    service = host.service("apache2")
-
-    assert service.is_enabled
-    assert service.is_running
+    assert host.package("apache2").is_installed
 
 
 def test_apache2_etc(host):
@@ -36,3 +22,24 @@ def test_apache2_logs(host):
         log = host.file(filename)
         assert log.exists
         assert log.is_file
+
+
+def test_apache2_listener(host):
+    assert host.socket("tcp://0.0.0.0:80").is_listening
+
+
+def test_apache2_service(host):
+    service = host.service("apache2")
+
+    assert service.is_enabled
+    assert service.is_running
+
+
+def test_apache2_connection(host):
+    print(host)
+    conn = http.client.HTTPConnection(host, 80, timeout=5)
+    conn.request("GET", "/")
+    response = conn.getresponse()
+    print(response.status)
+
+    assert response.status == "200"
