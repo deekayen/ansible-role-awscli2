8cebe0715693539e06740114ac033475f6b51423
David Norman <deekayen@deekayen.net>
2019-12-12 16:32:11 -0500

    Try to use urllib instead of http.client for connection test via python2.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,6 +1,6 @@
-import http.client
 import os
 import testinfra.utils.ansible_runner
+import urllib
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
@@ -37,9 +37,7 @@ def test_apache2_service(host):
 
 def test_apache2_connection(host):
     print(host)
-    conn = http.client.HTTPConnection(host, 80, timeout=5)
-    conn.request("GET", "/")
-    response = conn.getresponse()
-    print(response.status)
-
-    assert response.status == "200"
+    request = urllib.urlopen(host)
+    code = request.getcode()
+    print(code)
+    assert code == "200"
