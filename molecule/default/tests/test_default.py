926c0c140ad326e173a9f8a904c3f6f535c7155e
David Norman <deekayen@deekayen.net>
2019-12-12 13:57:15 -0500

    Try adding a http connection test. Remove testing on older distros. Rename existing tests.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,35 +1,34 @@
 import os
-
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_httpd_installed(host):
+def test_apache2_installed(host):
     apache = host.package("apache2")
 
     assert apache.is_installed
 
 
-def test_httpd_listener(host):
+def test_apache2_listener(host):
     listener = host.socket("tcp://0.0.0.0:80")
 
     assert listener.is_listening
 
 
-def test_httpd_service(host):
+def test_apache2_service(host):
     service = host.service("apache2")
 
     assert service.is_enabled
     assert service.is_running
 
 
-def test_httpd_etc(host):
+def test_apache2_etc(host):
     assert host.file("/etc/apache2").is_directory
 
 
-def test_httpd_logs(host):
+def test_apache2_logs(host):
     for filename in (
         ("/var/log/apache2/access.log"),
         ("/var/log/apache2/error.log"),
